cb7b50868612ef4cd5cee9c2373fff535e889ff3
sagesteppe <reedbenkendorf27@gmail.com>
2024-02-12 15:47:37 -0700

    fleshing out code to predict to surface

--- scripts/playGAM.R
+++ scripts/playGAM.R
@@ -30,38 +30,56 @@ spp <- terra::extract(preds, spp, bind = TRUE) |>
 
 splicies <- split(spp, f = spp$scntfcnm)
 splicies <- Filter(function(y) nrow(y) >= 50, splicies)
-lapply(X = splicies[40:50], modeller) 
+# lapply(X = splicies[40:50], modeller) 
 
-f <- paste0('../results/models/', list.files('../results/models/'))
-model <- readRDS(f[])
-
-gam.check(model)
-summary(model)
-plot(model)
-mat <- splicies[[3]] |> st_transform(5070)
 
 
 f <- file.path('../results/models', list.files('../results/models'))
-model <- readRDS(f[16])
+model <- readRDS(f[41])
 
 plot(model)
 abline(h=0)
 gam.check(model)
 summary(model)
 
+spp <- splicies[[which( gsub('.rds', '', basename(f[41])) == 
+                          gsub(' ', '_', names(splicies))  )]]
+
 # play with prediction
 
-# 1) make an aspatial prediction, determine start dates and end dates. 
-pred_df <- expand.grid(doy = seq(min(spp$doy), max(spp$doy), length.out = 25), 
-                       latitude = seq(min(spp$latitude), max(spp$latitude), length.out = 20), 
-                       bulk_density = seq(min(spp$bulk_density), max(spp$bulk_density), length.out = 20))
-pred_df$fit <- pred_df$fit <- predict(model, newdata = pred_df, type = 'response', se = F)
-pred_df$fit <- 1- pred_df$fit
+ob <- model$terms[[3]]
 
- # predict between these days onwards
-if({lowerDOY <- min(pred_df[ pred_df$fit > 0.1, ]$doy)} < 0){lowerDOY <- 0} else {
+spat_predict <- function(x){
+  
+  spp_range <- spp[spp$flowering==1,]
+  hist(spp_range$doy)
+  
+  pred_df <- expand.grid(
+    doy = seq(min(spp_range$doy)-14, max(spp_range$doy)+14, length.out = 25), # fixed, always here. 
+    bio10 = seq(min(spp_range$bio10), max(spp_range$bio10)), 
+    gddlgd10 = seq(min(spp_range$gddlgd10), max(spp_range$gddlgd10), length.out = 20)
+    )
+  
+  pred_df$fit <- pred_df$fit <- predict(model, newdata = pred_df, type = 'response', se = F)
+  plot(pred_df$doy, pred_df$fit)
+  
+}
+
+# 1) make an aspatial prediction, determine start dates and end dates. 
+spp_range <- spp[spp$flowering==1,]
+hist(spp_range$doy)
+pred_df <- expand.grid(doy = seq(min(spp_range$doy)-14, max(spp_range$doy)+14, length.out = 25), 
+                       bio10 = seq(min(spp_range$bio10), max(spp_range$bio10)), 
+                       gddlgd10 = seq(min(spp_range$gddlgd10), max(spp_range$gddlgd10), length.out = 20))
+
+pred_df$fit <- pred_df$fit <- predict(model, newdata = pred_df, type = 'response', se = F)
+plot(pred_df$doy, pred_df$fit)
+
+# identify first day with > 5% probability of flowering, peak, and last day with >10% flowering
+# predict between these days onwards
+if({lowerDOY <- min(pred_df[pred_df$fit > 0.1, ]$doy) } < 0){lowerDOY <- 0} else {
   lowerDOY <- floor(lowerDOY)}
-if({upperDOY <- max(pred_df[ pred_df$fit > 0.1, ]$doy)} > 365){upperDOY <- 365} else {
+if({upperDOY <- max(pred_df[ pred_df$fit > 0.1, ]$doy) } > 365){upperDOY <- 365} else { 
   upperDOY <- ceiling(upperDOY)}
 
 # predict only on these days. 
@@ -71,81 +89,85 @@ timeStamps <- round(seq(lowerDOY, upperDOY, by = 14)) # biweekly time stamps for
 # will be written to disk, and then these will be reloaded. 
 
 r1 <- rast(preds)[[1]]
-# s <- rast(c(replicate(length(timeStamps), r1)))
+names(r1) <- 'doy'
+taxon <- gsub('.rds', '', basename(f[41]))
+p1 <- file.path('../data/processed/timestamps', taxon)
 
+dir.create(file.path(p1, 'doy_constants'), showWarnings = FALSE, recursive = T)
 for (i in seq_along(1:length(timeStamps))){
-  r_fill <- setValues(r1, timeStamps[i])
-  r_fill <- terra::mask(r_fill, preds[[1]])
-  writeRaster(r_fill, )
   
+  r_fill <- terra::setValues(r1, timeStamps[i])
+  r_fill <- terra::mask(r_fill, preds[[1]])
+  
+  terra::writeRaster(r_fill, paste0(p1, '/doy_constants/', timeStamps[[i]],'.tif'), overwrite = T)
+  rm(r_fill) 
 }
 
-plot(s)
-names(r1) <- 'doy'
+# reload the rasters into a stack - ensure they are in ascending DOY Order. 
 
-ts1 <- c(preds, r1)
-doy149 <- terra::predict(ts1, model, type="response") 
-doy149 <- app(doy149, function(x){1-x})
+orderLoad <- function(path){
+  doy_f <- list.files(path)
+  doy_stack <- paste0(path, 
+                      sort(as.numeric(gsub('.tif', '', 
+                                             basename(doy_f)))), '.tif')
+  doy_stack <- terra::rast(lapply(doy_stack, terra::rast))
+  return(doy_stack)
+}
 
+doy_stack <- orderLoad(paste0(p1, '/doy_constants/'))
 
-plot(doy149)
-points(spp)
-# rip out the terms here to subset raster stack... 
-acle_model$gam$formula  # do this
+# predict the probability of the species flowering at each time point
+dir.create(file.path(p1, 'doy_preds'), showWarnings = FALSE)
+for (i in seq_along(1:length(timeStamps))){
+  
+  space_time <- c(preds, doy_stack[[i]])
+  time_pred <- terra::predict(space_time, model, type="response") 
+ # time_pred <- terra::app(time_pred, function(x){1-x})
+  
+  names(time_pred) <- timeStamps[[i]]
+  msk <- terra::ifel(time_pred < 0.01, NA, time_pred)
+  time_pred <- terra::mask(time_pred, msk)
+  
+  NACount <- freq(is.na(time_pred))
+  NACount <- NACount[ NACount$value==1, 'count'] / (dim(time_pred)[1]*dim(time_pred)[2])
+  
+  if(NACount < 0.99){
+  terra::writeRaster(time_pred, 
+                     paste0(p1, '/doy_preds/', timeStamps[[i]],'.tif'), overwrite = T)
+  } else {message('> 99% of Cells are NA, not writing layer ',  i, ' to disk.')}
+  
+  rm(time_pred)
+  terra::tmpFiles(current = FALSE, orphan = TRUE, old = TRUE, remove = TRUE)
+}
+
+pred_stack <- orderLoad(paste0(p1, '/doy_preds/'))
+plot(pred_stack)
+
+# isolate the initiation of flowering (10%), peak (max value), and cessation (90%) for each cell. 
 
 # subset the prediction surface to areas with suitable habitat
 
-# extract the relevant variables for each occupied pixel
 
-# create prediction data.frame using observed values and full DOY Range for each cell
 
-# identify first day with > 5% probability of flowering, peak, and last day with >10% flowering
+
+
+
+
+
+
+
+
+
+
+
+
 
 # write 9 rasters, 1) FIRST , 2) PEAK, 3) LAST, and each with it's upper and lower SE. 
 model
 
-spp <- splicies[[which( gsub('.shp', '', basename(f[25])) == 
-                          gsub(' ', '_', names(splicies))  )]]
 
 f[25] # bidens frondosa. 
 
-predictWRAP <- function(x, y){
-  
-  
-}
-
-ob145 <- terra::predict(preds, model, const = data.frame(doy = 145), type="response") 
-ob152 <- terra::predict(preds, model, const = data.frame(doy = 152), type="response") 
-ob159 <- terra::predict(preds, model, const = data.frame(doy = 159), type="response") 
-ob166 <- terra::predict(preds, model, const = data.frame(doy = 166), type="response") 
-ob225 <- terra::predict(preds, model, const = data.frame(doy = 225), type="response") 
-ob280 <- terra::predict(preds, model, const = data.frame(doy = 280), type="response") 
-
-
-ob_lyrs <-  terra::predict(preds, model, 
-                           const = data.frame(doy = c(145, 159, 172)), type="response") 
-
-plot(ob100) # these should all be 0
-plot(ob225) # these should all be 1
-plot(ob310) # these should all be 0
-
-ob145_inv <- app(ob145, function(x){1-x})
-ob152_inv <- app(ob152, function(x){1-x})
-ob159_inv <- app(ob159, function(x){1-x})
-ob166_inv <- app(ob166, function(x){1-x})
-ob225_inv <- app(ob225, function(x){1-x})
-ob280_inv <- app(ob280, function(x){1-x})
-
-plot(ob145_inv)
-plot(ob152_inv)
-plot(ob159_inv)
-plot(ob166_inv)
-plot(ob225_inv)
-
-# out <- 1- out 
-
-plot(out)
-points(vect(spp))
 
 
 ggplot(pred_df, aes(x = doy, y = fit)) + 
